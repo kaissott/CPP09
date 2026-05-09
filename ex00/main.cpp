237c4f0b1a736b437d8fd5299ecfe02628ffd060
kaissott <ramirez.kaiss@icloud.com>
2026-05-09 19:46:55 +0200

    refactor: Improve date validation logic and update code formatting
    - Enhanced `checkDateFormat` to include leap year and range checks for days and months.
    - Updated `openInputFile` and `main` logic for better readability.
    - Removed unneeded compiler flag `-Werror` from the `Makefile`.

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,41 +1,97 @@
-#include <tuple>
+
+#include <cstdlib>
 
 #include "BitcoinExchange.hpp"
 
-void	checkDateFormat(std::string date, char sep) {
+void	checkDateFormat(std::string date) {
 
-	std::string dayIntMonth[12] = {"31", "28", "31", "30", "31", "30", "31", "31", "30", "31", "30", "31"};
+	int daysIntMonths[12] = {31, 28, 31, 30,
+							31, 30, 31, 31,
+							30, 31, 30, 31};
 	bool		leapYear = false;
 
+	std::stringstream ss(date);
 	std::string year;
 	std::string month;
 	std::string day;
-	std::string tmp;
 
-	std::stringstream	ss(date);
-
-	while (std::getline(ss, tmp, sep)) {
-		
+	if (!std::getline(ss, year, '-')) {
+		std::cout << "Error: bad input ==> "<< date << std::endl;
+		return;
 	}
 
+	if (!std::getline(ss, month, '-')) {
+		std::cout << "Error: bad input ==> " << date << std::endl;
+		return;
+	}
 
-}
+	if (!std::getline(ss, day)) {
+		std::cout << "Error: bad input ==> " << date << std::endl;
+		return;
+	}
 
-void	checkLineInputFormat(BitcoinExchange &btc, std::string line) {
+	for (size_t i = 0; i < year.size(); ++i) {
+		if (!std::isdigit(year[i])) {
+			std::cout << "Error: bad input ==> " << date << std::endl;
+			return;
+		}
+	}
+	for (size_t i = 0; i < month.size(); ++i) {
+		if (!std::isdigit(month[i])) {
+			std::cout << "Error: bad input ==> " << date << std::endl;
+			return;
+		}
+	}
+	for (size_t i = 0; i < day.size(); ++i) {
+		if (!std::isdigit(day[i])) {
+			std::cout << "Error: bad input ==> " << date << std::endl;
+			return;
+		}
+	}
 
+	int tmp = std::atoi(year.c_str());
+	if (tmp < 0)
+		return;
+	if ((tmp % 4 == 0 && tmp % 100 != 0) || tmp % 400 == 0) {
+		leapYear = true;
+	}
 
+	tmp = std::atoi(month.c_str());
+	if (tmp < 1 || tmp > 12) {
+		std::cerr << "Error: bad input ==> " << date << std::endl;
+		return;
+	}
+	int m = tmp - 1;
+
+	tmp = std::atoi(day.c_str());
+	if (leapYear == true && m == 1) {
+		if (tmp > 29)
+			return;
+	}
+	else if (tmp < 0 || tmp > daysIntMonths[m]) {
+		std::cerr << "Error: bad input ==> " << date << std::endl;
+		return;
+	}
+	std::cout << "Good input ==> " << date << std::endl;
 }
 
 void	openInputFile(BitcoinExchange &btc, const char *inputFile) {
 
-	std::ifstream file(inputFile);
+	(void)btc;
+	(void)inputFile;
+	std::ifstream file("data.csv");
 	if (file.fail())
 		throw BitcoinExchange::errorOpen();
 
-
 	std::string line;
+	std::string date;
+	std::string value;
 	while (std::getline(file, line)) {
-		checkLineFormat(btc, line);
+		size_t sep = line.find(',');
+		date = line.substr(0, sep);
+		value = line.substr(sep + 1);
+		// std::cout << date << std::endl;
+		checkDateFormat(date);
 	}
 }
 
@@ -43,9 +99,8 @@ void	openInputFile(BitcoinExchange &btc, const char *inputFile) {
 int	main(int ac, char* av[]) {
 
 	if (ac == 3) {
-		BitcoinExchange btc();
+		BitcoinExchange btc;
 		try {
-			// openData();
 			openInputFile(btc, av[2]);
 		}
 		catch (std::exception &e) {
